2736f4b3d10c2876a83f5a350206463217fd2087
Nandini <nandinivetcha17@gmail.com>
2026-06-23 14:06:57 +0530

    Initial Commit

--- backend/__init__.py
+++
@@ -1,2 +0,0 @@
-# backend package initializer
-# This file allows Flask routes to be organized in the backend package.
--- database/__init__.py
+++
@@ -1,2 +0,0 @@
-# database package initializer
-# This file allows Python to treat the database folder as a module.
